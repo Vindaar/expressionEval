c948a0485de5041288bdfd1b9ef0e3e0ba53be6f
Vindaar <basti90@gmail.com>
2021-03-16 13:28:43 +0100

    add basic working implementation of the library
    Probably a bunch of bad design choices due to my lack of C++
    experience in recent years...

---
+++ playground/test_expression.cpp
@@ -0,0 +1,11 @@
+#include "../expression_eval.h"
+
+int main() {
+    std::string s = "hitsAna_energy > 5000 + 100 && hitsAna_xy2Sigma < 0.5";
+    auto ast = parseExpression(s);
+    std::cout << "Final AST: " << astToStr(ast) << std::endl;
+
+    std::map<std::string, float> m = { {"hitsAna_energy", 6000}, {"hitsAna_xy2Sigma", 0.2} };
+    std::cout << "Eval = " << evaluate(m, ast).getRight() << std::endl;
+
+}
